1b09e8eb83f8695109f7a5f6c4c41491f29eb81e
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-11-03 14:37:06 -0300

    Created script to facilitate download of datasets

---
+++ scripts/download_raw_data.py
@@ -0,0 +1,10 @@
+"""
+For more details: https://www.kaggle.com/docs/api
+Tip: you must move the dataset to datas folder!
+"""
+
+import kagglehub
+
+if __name__=="__main__":
+    kagglehub.dataset_download("adityakadiwal/water-potability") # Classification data
+    # Regression data download here: https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data
